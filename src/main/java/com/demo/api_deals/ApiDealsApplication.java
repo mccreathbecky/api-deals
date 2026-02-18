21b69523b5fc87e5bb8f2edb1411f34096b5e7e0
Becky McCreath <mccreathbecky@gmail.com>
2026-02-18 16:17:13 +1100

    setup controller with placeholder values, verify app runs

--- src/main/java/com/demo/api_deals/ApiDealsApplication.java
+++ src/main/java/com/demo/api_deals/ApiDealsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class ApiDealsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ApiDealsApplication.class, args);
 
         System.out.println("The application ran successfully!");
     }
